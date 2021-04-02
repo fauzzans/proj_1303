d42ff7f4a4e759ffde6e552b7e79983185c1b589
alghifari abudzar <r.abudzar@gmail.com>
2021-04-02 14:44:27 +0700

    cetak form pendaftaran, invoice

--- application/views/kop_lap.php
+++ application/views/kop_lap.php
@@ -1,22 +1,22 @@
 <table border="0" width="100%">
   <tr>
     <td align="center">
-      <img src="img/logo2.png" alt="logo2" width="60">
+      <img src="img/logo2.png" alt="logo2" width="70">
     </td>
     <td align="center">
-      <b style="font-size:23px;">PANITIA PENERIAMAAN PESERTA DIDIK BARU (PPDB)</b> <br>
-      <b style="font-size:30px;">SMA NEGERI 1 BELITANG</b> <br>
-      <b style="font-size:20px;">TAHUN PELAJARAN <?php echo $thn_ppdb; ?> / <?php echo $thn_ppdb+1; ?></b>
+      <b style="font-size:16px;">PANITIA PENERIAMAAN PESERTA DIDIK BARU (PPDB)</b> <br>
+      <b style="font-size:23px;">MADRASAH TSANAWIYAH (MTS) DARUT TAFSIR</b> <br>
+      <b style="font-size:13px;">TAHUN PELAJARAN <?php echo $thn_ppdb; ?> / <?php echo $thn_ppdb + 1; ?></b>
     </td>
     <td align="center">
       <img src="img/logo.png" alt="logo2" width="70">
     </td>
   </tr>
   <tr>
-    <td colspan="3" align="center" style="font-size:15px;">
-      Sekretariat : JL.Marga Pemuka Bangsa Raja No.1001 Gumawang Belitang <img src="img/telp.jpg" alt="telp." style="margin-bottom:-5px;margin-right:-5px;"> 0735-450106 Kab.OKU Timur <img src="img/kode_pos.jpg" alt="Kode Pos." style="margin-bottom:-3px;margin-right:-5px;"> 32382
+    <td colspan="3" align="center" style="font-size:12px;">
+      Sekretariat : Kampung Pabuaran Kaum, RT.3/RW.2, Cibanteng, Kec. Ciampea, Bogor, Jawa Barat 16620 Telp.0813-9973-0940
       <br>
-      Website : https://ppdb.sman1belitang.sch.id   e-mail : smanegeri1belitang@gmail.com
+      Website : https://pondokpesantrendaruttafsir.com e-mail : ponpestrendaruttafsir@gmail.com
     </td>
   </tr>
   <tr>
--- application/views/siswa/cetak.php
+++ application/views/siswa/cetak.php
@@ -1,216 +1,333 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title><?php echo $judul_web; ?></title>
-    <base href="<?php echo base_url();?>"/>
-  	<link rel="icon" type="image/png" href="assets/images/favicon.png"/>
-    <style>
+
+<head>
+  <meta charset="utf-8">
+  <title><?php echo $judul_web; ?></title>
+  <base href="<?php echo base_url(); ?>" />
+  <link rel="icon" type="image/png" href="assets/images/favicon.png" />
+  <style>
     table {
-        border-collapse: collapse;
+      border-collapse: collapse;
     }
-    thead > tr{
+
+    thead>tr {
       background-color: #0070C0;
-      color:#f1f1f1;
+      color: #f1f1f1;
     }
-    thead > tr > th{
+
+    thead>tr>th {
       background-color: #0070C0;
-      color:#fff;
+      color: #fff;
       padding: 10px;
       border-color: #fff;
     }
-    th, td {
+
+    th,
+    td {
       padding: 2px;
     }
 
     th {
-        color: #222;
+      color: #222;
     }
-    body{
-      font-family:Calibri;
+
+    body {
+      font-family: Calibri;
     }
-    </style>
-  </head>
-  <body onload="window.print();">
-    <?php $this->load->view('kop_lap'); ?>
-    <h4 align="center" style="margin-top:0px;"><u>BUKTI PENDAFTARAN</u></h4>
-    <b><center>
-      PANITIA PENERIAMAAN PESERTA DIDIK BARU (PPDB) <br>
-      SMA NEGERI 1 BELITANG <br>
-      TAHUN PELAJARAN <?php echo $thn_ppdb; ?> / <?php echo $thn_ppdb+1; ?></center>
-    </b>
-    <br>
+  </style>
+</head>
 
-    <table width="100%" border="0">
-      <tr>
-        <td width="200">NO. PENDAFTARAN</td>
-        <td width="1">:</td>
-        <td><?php echo $user->no_pendaftaran; ?></td>
-      </tr>
-      <tr>
-        <td>TANGGAL PENDAFTARAN </td>
-        <td>:</td>
-        <td><?php echo $this->Model_data->tgl_id(date('d-m-Y', strtotime($user->tgl_siswa))); ?></td>
-      </tr>
-      <tr>
-        <td>TANGGAL CETAK </td>
-        <td>:</td>
-        <td><?php echo $this->Model_data->tgl_id(date('d-m-Y')); ?></td>
-      </tr>
-      <tr>
-        <td>NIS</td>
-        <td>:</td>
-        <td><?php echo $user->nis; ?></td>
-      </tr>
-      <tr>
-        <td>NISN</td>
-        <td>:</td>
-        <td><?php echo $user->nisn; ?></td>
-      </tr>
-      <tr>
-        <td>NIK</td>
-        <td>:</td>
-        <td><?php echo $user->nik; ?></td>
-      </tr>
-      <tr>
-        <td>NAMA LENGKAP</td>
-        <td>:</td>
-        <td><?php echo ucwords($user->nama_lengkap); ?></td>
-      </tr>
-      <tr>
-        <td>JENIS KELAMIN</td>
-        <td>:</td>
-        <td><?php echo $user->jk; ?></td>
-      </tr>
-      <tr>
-        <td>TEMPAT, TANGGAL LAHIR</td>
-        <td>:</td>
-        <td><?php echo "$user->tempat_lahir, ".$this->Model_data->tgl_id($user->tgl_lahir); ?></td>
-      </tr>
-      <tr>
-        <td>AGAMA</td>
-        <td>:</td>
-        <td><?php echo $user->agama; ?></td>
-      </tr>
-      <tr>
-        <td>NAMA ORANG TUA /WALI</td>
-        <td>:</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td style="padding-left:55px;">AYAH</td>
-        <td>:</td>
-        <td><?php echo ucwords($user->nama_ayah); ?></td>
-      </tr>
-      <tr>
-        <td style="padding-left:55px;">IBU</td>
-        <td>:</td>
-        <td><?php echo ucwords($user->nama_ibu); ?></td>
-      </tr>
-      <tr>
-        <td style="padding-left:55px;">WALI</td>
-        <td>:</td>
-        <td><?php echo ucwords($user->nama_wali); ?></td>
-      </tr>
-      <tr>
-        <td>NO. HANDPHONE (HP)</td>
-        <td>:</td>
-        <td><?php echo $user->no_hp_siswa; ?></td>
-      </tr>
-      <tr>
-        <td>ASAL SEKOLAH</td>
-        <td>:</td>
-        <td><?php echo ucwords($user->nama_sekolah); ?></td>
-      </tr>
-      <tr>
-        <td>RATA - RATA NILAI RAPOR</td>
-        <td>:</td>
-        <td><?php echo number_format($nilai_rapor,2,",","."); ?></td>
-      </tr>
-      <tr>
-        <td>RATA - RATA NILAI USBN</td>
-        <td>:</td>
-        <td><?php echo number_format($nilai_usbn,2,",","."); ?></td>
-      </tr>
-      <tr>
-        <td>RATA - RATA NILAI UNBK/UNKP</td>
-        <td>:</td>
-        <td><?php echo number_format($nilai_unbk,2,",","."); ?></td>
-      </tr>
-    </table>
-    <br>
+<body onload="window.print();">
+  <?php $this->load->view('kop_lap'); ?>
+  <h4 align="center" style="margin-top:0px;"><u>BUKTI PENDAFTARAN</u></h4>
+  <center>
+    PANITIA PENERIAMAAN PESERTA DIDIK BARU (PPDB) <br>
+    MADRASAH TSANAWIYAH (MTS) DARUT TAFSIR <br>
+    TAHUN PELAJARAN <?php echo $thn_ppdb; ?> / <?php echo $thn_ppdb + 1; ?></center>
+  <table width="100%" border="0">
+    <tr>
+      <td style="width: 150px;">SISWA</td>
+    </tr>
+    <tr>
+      <td>No.Pendaftaran</td>
+      <td>:</td>
+      <td><?php echo $user->no_pendaftaran; ?></td>
+    </tr>
+    <tr>
+      <td>Tanggal Pendaftaran</td>
+      <td>:</td>
+      <td><?php echo $this->Model_data->tgl_id(date('d-m-Y', strtotime($user->tgl_siswa))); ?></td>
+    </tr>
+    <tr>
+      <td>Tanggal Cetak</td>
+      <td>:</td>
+      <td><?php echo $this->Model_data->tgl_id(date('d-m-Y')); ?></td>
+    </tr>
+    <tr>
+    <tr>
+      <td>NIK</td>
+      <td>:</td>
+      <td><?php echo $user->nik; ?></td>
+    </tr>
+    <tr>
+      <td>Nama Lengkap</td>
+      <td>:</td>
+      <td><?php echo ucwords($user->nama_lengkap); ?></td>
+    </tr>
+    <tr>
+      <td>Jenis Kelamin</td>
+      <td>:</td>
+      <td><?php echo $user->jk; ?></td>
+    </tr>
+    <tr>
+      <td>Tempat Tanggal Lahir</td>
+      <td>:</td>
+      <td><?php echo "$user->tempat_lahir, " . $this->Model_data->tgl_id($user->tgl_lahir); ?></td>
+    </tr>
+    <tr>
+      <td>Anak Ke (berapa)</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Jumlah Saudara</td>
+      <td>:</td>
+      <td>Kandung </td>
+      <td>Berat Badan</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td>:</td>
+      <td>Tiri</td>
+      <td>Tinggi</td>
+    </tr>
+    <tr>
+      <td>Alamat Lengkap</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td>Desa/Kelurahan</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td>Kab/Kota</td>
+      <td></td>
+      <td>Kode Pos</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td>Telp/HP</td>
+      <td></td>
+    </tr>
+    <tr>
+      <!-- hubungan keluarga -->
+      <td>Keterangan</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+  </table>
+  <br>
+  <table style="page-break-after: always;">
+    <tr>
+      <td>ORANG TUA / WALI</td>
+    </tr>
+    <tr>
+      <td>Orang Tua</td>
+      <td>:</td>
+      <td>Ayah</td>
+      <td><?php echo ucwords($user->nama_ayah); ?></td>
+      <td>Ibu</td>
+      <td><?php echo ucwords($user->nama_ibu); ?></td>
+    </tr>
+    <tr>
+      <td>Tempat Tanggal Lahir</td>
+      <td>:</td>
+      <td>Ayah</td>
+      <td></td>
+      <td>Ibu</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Pekerjaan</td>
+      <td>:</td>
+      <td>Ayah</td>
+      <td></td>
+      <td>Ibu</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Pendidikan</td>
+      <td>:</td>
+      <td>Ayah</td>
+      <td></td>
+      <td>Ibu</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Agama</td>
+      <td>:</td>
+      <td>Ayah</td>
+      <td></td>
+      <td>Ibu</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Penghasilan</td>
+      <td>:</td>
+      <td>Ayah</td>
+      <td>Rp</td>
+      <td>Ibu</td>
+      <td>Rp</td>
+    </tr>
+    <!-- WALI -->
+    <tr>
+      <td>Nama Wali</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Pekerjaan</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Pendidikan</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Agama</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Hubungan dengan anak</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Alamat Lengkap</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+  </table>
 
-    <div style="float:right;">
-      OKU Timur, <?php echo $this->Model_data->tgl_id(date('d-m-Y')); ?> <br>
-			Ketua Panitia PPDB,  <br>
-      <img src="img/ttd.jpg" alt="" width="100"><br>
-      <b><u>KETUT SUDIARTE, S.Pd.</u></b><br>
-      NIP. 197001301997031006
-    </div>
-    <br><br><br><br><br><br><br><br><br><br>
+  <table style="page-break-before:always">
+    <tr>
+      <td>Asal Sekolah</td>
+    </tr>
+    <tr>
+      <td>Nama Sekolah/Madrasah</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Ijazah Nomor</td>
+      <td>:</td>
+      <td></td>
+      <td>Tahun</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Nomor Ujian</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>NISN</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Alamat Sekolah</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Tanggal Masuk</td>
+      <td>:</td>
+      <td></td>
+    </tr>
+  </table>
 
-    <b><u>Siapkan Berkas Berikut Ketika anda melakukan verifikasi :</u></b>
-    <br>
-    <table width="100%" border="0" style="margin-left:5px;">
-      <tr>
-        <td width="1">1.</td>
-        <td>Cetak bukti pendaftaran</td>
-        <td width="1">:</td>
-        <td>1 lembar</td>
-      </tr>
-      <tr>
-        <td>2.</td>
-        <td>Cetak rekap nilai</td>
-        <td>:</td>
-        <td>1 lembar</td>
-      </tr>
-      <tr>
-        <td>3.</td>
-        <td>Pas foto berwarna ukuran 3 x 4</td>
-        <td>:</td>
-        <td>3 lembar</td>
-      </tr>
-      <tr>
-        <td>4.</td>
-        <td>Print out ASLI NISN dari web  <i>http://nisn.data.kemdikbud.go.id</i> Dilegalisir</td>
-        <td>:</td>
-        <td>1 lembar</td>
-      </tr>
-      <tr>
-        <td>5.</td>
-        <td>Foto copy rapor SMP/MTs. Semester 1 s.d 5 Dilegalisir</td>
-        <td>:</td>
-        <td>1 lembar</td>
-      </tr>
-      <tr>
-        <td>6.</td>
-        <td>Foto copy nilai USBN Dilegalisir</td>
-        <td>:</td>
-        <td>1 lembar</td>
-      </tr>
-      <tr>
-        <td>7.</td>
-        <td>Foto copy nilai UNKP / UNBK Dilegalisir</td>
-        <td>:</td>
-        <td>1 lembar</td>
-      </tr>
-      <tr>
-        <td>8.</td>
-        <td>Fotocopy Dakol Nem Dilegalisir</td>
-        <td>:</td>
-        <td>1 lembar</td>
-      </tr>
-      <tr>
-        <td>9.</td>
-        <td>Foto copy Kartu Keluarga KK</td>
-        <td>:</td>
-        <td>1 lembar</td>
-      </tr>
-      <tr>
-        <td valign="top">10.</td>
-        <td colspan="3">Semua berkas dimasukan kedalam map plastik berlubang (Contoh:  merk folder one), laki – laki warna merah dan perempuan warna biru</td>
-      </tr>
-    </table>
+  <table>
+    <tr style="width: 300px;height: 150px;">
+      <td style="width: 300px;"><br>
+        Tanda Tangan Orang Tua,
+      </td>
+      <td style="width: 300px;"></td>
+      <td style="width: 300px;">Bogor, <?php echo $this->Model_data->tgl_id(date('d-m-Y')); ?> <br>
+        Tanda Tangan Calon Siswa/Santri,
+      </td>
+    </tr>
+    <tr>
+      <td style="border-top:solid;width: 300px;">
+        Nama
+      </td>
+      <td style="width: 300px;"></td>
+      <td style="border-top:solid;width: 300px;">
+        Nama
+      </td>
+    </tr>
+  </table>
 
-  </body>
+  <br><br>
+
+  <b><u>Siapkan Berkas Berikut Ketika anda melakukan verifikasi :</u></b>
+  <br>
+  <table width="100%" border="0" style="margin-left:5px;">
+    <tr>
+      <td width="1">1.</td>
+      <td>Cetak bukti pendaftaran</td>
+      <td width="1">:</td>
+      <td>1 lembar</td>
+    </tr>
+    <tr>
+      <td>2.</td>
+      <td>Photo Copy Ijazah</td>
+      <td>:</td>
+      <td>3 lembar</td>
+    </tr>
+    <tr>
+      <td>3.</td>
+      <td>Surat Keterangan Hasil Ujian (SKHUN)</td>
+      <td>:</td>
+      <td>ASLI</td>
+    </tr>
+    <tr>
+      <td>4.</td>
+      <td>Buku Raport</td>
+      <td>:</td>
+      <td>ASLI</td>
+    </tr>
+    <tr>
+      <td>5.</td>
+      <td>NISN</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>6.</td>
+      <td>Pas Photo Ukuran 3 x 4</td>
+      <td>:</td>
+      <td>6 lembar</td>
+    </tr>
+    <tr>
+      <td>7.</td>
+      <td>Pas Photo Ukuran 2 x 3</td>
+      <td>:</td>
+      <td>3 lembar</td>
+    </tr>
+  </table>
+
+</body>
+
 </html>
